6106ee94f9dd1d905eb3512f2d350e45956e8fdd
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2023-11-08 00:01:00 +0600

    added cookieParser

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
     credentials: true,
 }))
 app.use(express.json())
-app.use(cookieParser())
+app.use(cookieParser)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.glcj3l3.mongodb.net/?retryWrites=true&w=majority`;
 
